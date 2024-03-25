bd23efd1e35ee4fbfbe6e2deb093d4a23f6cdcd7
Danylo Altukhov <dan.97198@gmail.com>
2024-03-25 16:44:55 +0200

    fix problem with mail

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,21 +1,24 @@
 const nodemailer = require("nodemailer");
 
-const { META_PASS } = process.env;
+const { MAIL_PASS, MAIL_EMAIL } = process.env;
 
 const nodemailerConfig = {
-  host: "smtp.meta.ua",
+  host: "smtp.ukr.net",
   port: 465,
   secure: true,
   auth: {
-    user: "cheekygeroin@meta.ua",
-    pass: META_PASS,
+    user: MAIL_EMAIL,
+    pass: MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 };
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "cheekygeroin@meta.ua" };
+  const email = { ...data, from: "cheekygeroin@ukr.net" };
   await transport.sendMail(email);
   return true;
 };
